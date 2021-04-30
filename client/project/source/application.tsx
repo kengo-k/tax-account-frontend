84caeba83450f2f332d85f3b1169d8728830f352
kurobanekengo <kurobanekengo@gmail.com>
2021-04-30 16:02:34 +0900

    不要処理削除

--- client/project/source/application.tsx
+++ client/project/source/application.tsx
@@ -1,9 +1,6 @@
 import * as React from "react";
 import { useModule } from "@module/action";
-import { getConfig } from "@config/Config";
 import { Main } from "@component/Main";
-
-console.log(getConfig());
 
 export const Application = () => {
   useModule();
