da561835223041c986221ade8ced9cdf807948b6
kurobanekengo <kurobanekengo@gmail.com>
2021-05-02 18:54:48 +0900

    #7 台帳APIを追加

--- client/project/source/api/presentation/PresentationApi.ts
+++ client/project/source/api/presentation/PresentationApi.ts
@@ -2,4 +2,5 @@ import { get } from "@api/ApiBase";
 
 export const PresentationApi = {
   selectInit: get(1, "papi", "init", (req) => req),
+  selectLedger: get(1, "api", "ledger/:nendo/:ledgerCd", (req) => req),
 };
