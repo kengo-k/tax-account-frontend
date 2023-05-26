99b0d3a4fef88e1588cfa635cdebd837546e1f46
kengo-k <31839234+kengo-k@users.noreply.github.com>
2023-05-26 23:26:18 +0900

    外部reactへの参照を削除

--- webpack.config.js
+++ webpack.config.js
@@ -50,19 +50,5 @@ module.exports = {
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(environment),
     }),
-  ],
-  externals: {
-    react: {
-      commonjs: 'react',
-      commonjs2: 'react',
-      amd: 'react',
-      root: 'React',
-    },
-    'react-dom': {
-      commonjs: 'react-dom',
-      commonjs2: 'react-dom',
-      amd: 'react-dom',
-      root: 'ReactDOM',
-    },
-  },
+  ]
 };
